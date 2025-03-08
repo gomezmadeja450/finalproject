ce59a0726082e31a46a6e727ff9916fa3ea17ef6
gomezmadeja450 <gomezmadeja450@gmail.com>
2025-03-08 07:39:00 +0000

    Add 3.c

---
+++ src/3.c
@@ -0,0 +1,9 @@
+  int main(){
+    int x=10;
+    if(x>5){
+      printf("The value of x is greater than 5");
+    }else{
+      printf("The value of x is less than or equal to 5");
+    }
+    return 0;
+  }
