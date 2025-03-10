c2df76467e089aa6ab4ddd51709dc0e96377459c
gomezmadeja450 <gomezmadeja450@gmail.com>
2025-03-10 16:21:33 +0000

    Add 5.c

---
+++ src/5.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    int x = 0;
+    int y = 123456789;
+    if (x > y) {
+        printf("x is greater than y\n");
+    } else {
+        printf("x is less than or equal to y\n");
+    }
+    return 0;
+}
