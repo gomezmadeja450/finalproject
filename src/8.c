940435f9c676d97f9dcbcf06fe010b5a3047e563
gomezmadeja450 <gomezmadeja450@gmail.com>
2025-03-13 11:06:43 +0000

    Add 8.c

---
+++ src/8.c
@@ -0,0 +1,12 @@
+// FinalProject.c
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main() {
+  int number = 12; // Change this to a different integer value
+  char* str = "Hello, World!";
+  printf("%d %s\n", number, str);
+  return 0;
+}
