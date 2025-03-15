05da0e7d4de63df52ed04a78c84f7104285ff2c3
gomezmadeja450 <gomezmadeja450@gmail.com>
2025-03-15 22:51:20 +0000

    Add 10.c

---
+++ src/10.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    int x = 5;
+    float y = 3.14;
+    char z = 'a';
+    printf("The value of x is %d, the value of y is %f, and the value of z is %c\n", x, y, z);
+    return 0;
+}
