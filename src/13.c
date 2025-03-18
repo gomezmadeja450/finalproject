ddfa0fdba0e27101e81e75d9497554b870233fff
gomezmadeja450 <gomezmadeja450@gmail.com>
2025-03-18 16:05:22 +0000

    Add 13.c

---
+++ src/13.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main() {
+  int num1 = 42;
+  float num2 = 3.14;
+  char *str = "Hello World!";
+  printf("The value of num1 is: %d\n", num1);
+  printf("The value of num2 is: %f\n", num2);
+  printf("The string is: %s\n", str);
+  return 0;
+}
