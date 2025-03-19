55a3818d29d9d6dde3fd5065ab51249d3e56394e
gomezmadeja450 <gomezmadeja450@gmail.com>
2025-03-19 07:48:27 +0000

    Add 14.c

---
+++ src/14.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    int x = 5;
+    if (x == 1) {
+        printf("x is equal to 1\n");
+    } else if (x == 2) {
+        printf("x is equal to 2\n");
+    } else {
+        printf("x is not equal to 1 or 2\n");
+    }
+}
