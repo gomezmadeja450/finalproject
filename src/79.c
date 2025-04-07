29aa4fc7b3613945414e00d3db5ddb9fccb31012
gomezmadeja450 <gomezmadeja450@gmail.com>
2025-04-07 15:57:50 +0000

    Add 79.c

---
+++ src/79.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+void main() {
+    int i;
+
+    printf("Enter the number of rows: ");
+    scanf("%d", &i);
+
+    for (i = 1; i <= i; i++) {
+        printf("*");
+    }
+
+    printf("\n");
+}
