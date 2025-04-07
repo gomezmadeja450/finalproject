e44c0a8ed007e2aec80c816160ad05a0dc6601b0
gomezmadeja450 <gomezmadeja450@gmail.com>
2025-04-07 15:25:34 +0000

    Add 56.c

---
+++ src/56.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    int i;
+
+    printf("This is a C program.\n");
+    printf("The program uses scanf and printf statements to read and print integers.");
+    for (i = 0; i < 5; ++i) {
+        printf("%d ", i);
+    }
+    return 0;
+}
