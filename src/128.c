b65a732babf301bb92a115bddb790e70426aca01
gomezmadeja450 <gomezmadeja450@gmail.com>
2025-04-09 20:23:50 +0000

    Add 128.c

---
+++ src/128.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int x = 5;
+    int y = 10;
+
+    if (x > y) {
+        printf("x is greater than y\n");
+    } else {
+        printf("y is greater than x\n");
+    }
+
+    return 0;
+}
